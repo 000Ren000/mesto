3455ffb72df9be8443893856f9f2a8b3d532c763
000Ren000 <antonren@yandex.ru>
2022-03-05 14:18:29 +0700

    Изменил сохранение формы через сабмит

--- script.js
+++ script.js
@@ -1,30 +1,64 @@
 const popup = document.querySelector(".popup");
-const form = popup.querySelector(".edit-form");
 
-let entryName = form.querySelector(".edit-form__Name");
-let entryProfesion = form.querySelector(".edit-form__profession");
-let btnSave = form.querySelector(".edit-form__button-save");
-let btnClose = form.querySelector(".edit-form__button-close");
+const card = document.querySelector(".photo__cards");
+let listButtonlike = card.querySelectorAll(".card__button-like");
+
+// const form = popup.querySelector(".edit-form");
+// let entryName = form.querySelector(".edit-form__Name");
+// let entryProfesion = form.querySelector(".edit-form__profession");
 
 let editForm = document.querySelector(".profile__edit-button");
 let editName = document.querySelector(".profile__name");
 let editProfession = document.querySelector(".profile__profession");
 
-const card = document.querySelector(".photo__cards");
-let listButtonlike = card.querySelectorAll(".card__button-like");
-
 // задаем Имя и профессию по умолчанию
-entryName.value = editName.textContent;
-entryProfesion.value = editProfession.textContent;
+// entryName.value = editName.textContent;
+// entryProfesion.value = editProfession.textContent;
 
-// Сохранение введеных данных
-btnSave.addEventListener("click", saved);
+// // Сохранение введеных данных
+// btnSave.addEventListener("click", saved);
 
-function saved() {
-  editName.textContent = entryName.value;
-  editProfession.textContent = entryProfesion.value;
+// function saved() {
+//   editName.textContent = entryName.value;
+//   editProfession.textContent = entryProfesion.value;
+//   togleForm();
+// }
+
+// Находим форму в DOM
+let formElement = document.querySelector(".popup"); // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector(".edit-form__name"); // Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector(".edit-form__profession"); // Воспользуйтесь инструментом .querySelector()
+
+let btnSave = formElement.querySelector(".edit-form__button-save");
+let btnClose = formElement.querySelector(".edit-form__button-close");
+
+// Открытие формы
+function openPopup() {
+  jobInput.value = editProfession.textContent;
+  nameInput.value = editName.textContent;
   togleForm();
 }
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
+
+  // Получите значение полей jobInput и nameInput из свойства value
+  // Выберите элементы, куда должны быть вставлены значения полей
+
+  // Вставьте новые значения с помощью textContent
+  editName.textContent = nameInput.value;
+  editProfession.textContent = jobInput.value;
+  togleForm();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener("submit", formSubmitHandler);
 
 // //Поставить лайк
 listButtonlike.forEach((element) => {
@@ -34,18 +68,17 @@ listButtonlike.forEach((element) => {
 });
 
 // Закрытие окна
-btnClose.addEventListener("click", togleForm);
-editForm.addEventListener("click", togleForm);
-
+btnClose.addEventListener("click", function () {
+  popup.classList.remove("popup__opened");
+});
+editForm.addEventListener("click", openPopup);
+console.log(btnClose);
 function togleForm() {
   popup.classList.toggle("popup_opened");
 }
 
-popup.addEventListener("click", function (event) {
-  if (event.target === event.currentTarget) {
-    togleForm();
-  }
-});
-
-
-
+// popup.addEventListener("click", function (event) {
+//   if (event.target === event.currentTarget) {
+//     togleForm();
+//   }
+// });
