fa9e0159d488e72e64f5b3f6f119d71fbb062c02
000Ren000 <antonren@yandex.ru>
2022-03-06 16:29:35 +0700

    fix / функций открыть форму и закрыть

--- script.js
+++ script.js
@@ -1,85 +1,56 @@
-const popup = document.querySelector(".popup");
+const popup = document.querySelector('.popup');
 
-const card = document.querySelector(".photo__cards");
-let listButtonlike = card.querySelectorAll(".card__button-like");
+const card = document.querySelector('.photo__cards');
 
-// const form = popup.querySelector(".edit-form");
-// let entryName = form.querySelector(".edit-form__Name");
-// let entryProfesion = form.querySelector(".edit-form__profession");
-
-let editForm = document.querySelector(".profile__edit-button");
-let editName = document.querySelector(".profile__name");
-let editProfession = document.querySelector(".profile__profession");
-
-// задаем Имя и профессию по умолчанию
-// entryName.value = editName.textContent;
-// entryProfesion.value = editProfession.textContent;
-
-// // Сохранение введеных данных
-// btnSave.addEventListener("click", saved);
-
-// function saved() {
-//   editName.textContent = entryName.value;
-//   editProfession.textContent = entryProfesion.value;
-//   togleForm();
-// }
+let editForm = document.querySelector('.profile__edit-button');
+let editName = document.querySelector('.profile__name');
+let editProfession = document.querySelector('.profile__profession');
 
 // Находим форму в DOM
-let formElement = document.querySelector(".popup"); // Воспользуйтесь методом querySelector()
+let formElement = document.querySelector('.popup'); // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector(".edit-form__name"); // Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector(".edit-form__profession"); // Воспользуйтесь инструментом .querySelector()
+let nameInput = formElement.querySelector('.edit-form__name'); // Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector('.edit-form__profession'); // Воспользуйтесь инструментом .querySelector()
 
-let btnSave = formElement.querySelector(".edit-form__button-save");
-let btnClose = formElement.querySelector(".edit-form__button-close");
+let btnSave = formElement.querySelector('.edit-form__button-save');
+let btnClose = formElement.querySelector('.edit-form__button-close');
 
 // Открытие формы
 function openPopup() {
   jobInput.value = editProfession.textContent;
   nameInput.value = editName.textContent;
-  togleForm();
+  openForm();
 }
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+// Обработчик «отправки» формы
 function formSubmitHandler(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  // Так мы можем определить свою логику отправки.
-  // О том, как это делать, расскажем позже.
-
-  // Получите значение полей jobInput и nameInput из свойства value
-  // Выберите элементы, куда должны быть вставлены значения полей
-
-  // Вставьте новые значения с помощью textContent
+  evt.preventDefault();
   editName.textContent = nameInput.value;
   editProfession.textContent = jobInput.value;
-  togleForm();
+  closeForm();
 }
 
+//Открыть форму
+function openForm() {
+  popup.classList.add('popup_opened');
+}
+//Закрыть форму
+function closeForm() {
+  popup.classList.remove('popup_opened');
+}
 // Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener("submit", formSubmitHandler);
-
-// //Поставить лайк
-listButtonlike.forEach((element) => {
-  element.addEventListener("click", function () {
-    element.classList.toggle("card__button-like_active");
-  });
-});
+formElement.addEventListener('submit', formSubmitHandler);
 
 // Закрытие окна
-btnClose.addEventListener("click", function () {
-  popup.classList.remove("popup__opened");
+btnClose.addEventListener('click', closeForm);
+
+editForm.addEventListener('click', openPopup);
+
+popup.addEventListener('click', function (event) {
+  if (event.target === event.currentTarget) {
+    closeForm();
+  }
 });
 
-editForm.addEventListener("click", openPopup);
 
-function togleForm() {
-  popup.classList.toggle("popup_opened");
-}
 
-// popup.addEventListener("click", function (event) {
-//   if (event.target === event.currentTarget) {
-//     togleForm();
-//   }
-// });
