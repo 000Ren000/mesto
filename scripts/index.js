a18bf88fcd97129a83b70f770ebdb332a745a6f3
000Ren000 <antonren@yandex.ru>
2022-03-20 18:13:31 +0700

    Перевел форму редактирования профиля в отдельную функцию

--- scripts/index.js
+++ scripts/index.js
@@ -5,10 +5,10 @@ let editName = document.querySelector('.profile__name');
 let editProfession = document.querySelector('.profile__profession');
 
 // Находим форму в DOM
-let formElement = document.querySelector('.edit-form'); // Воспользуйтесь методом querySelector()
+let formElement = document.querySelector('#edit-form'); // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector('.edit-form__input_type_name'); // Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('.edit-form__input_type_profession'); // Воспользуйтесь инструментом .querySelector()
+// let nameInput = formElement.querySelector('.edit-form__input_type_name'); // Воспользуйтесь инструментом .querySelector()
+// let jobInput = formElement.querySelector('.edit-form__input_type_profession'); // Воспользуйтесь инструментом .querySelector()
 
 let btnClose = document.querySelector('.popup__button-close');
 
@@ -48,11 +48,11 @@ initialCards.forEach( crd => {
   card.querySelector('.card__title').textContent = crd.name;
   photoCards.append(card);})
 // Открытие формы
-function openPopup() {
-  jobInput.value = editProfession.textContent;
-  nameInput.value = editName.textContent;
-  popup.classList.add('popup_opened');
-}
+// function openPopup() {
+//   jobInput.value = editProfession.textContent;
+//   nameInput.value = editName.textContent;
+//   popup.classList.add('popup_opened');
+// }
 
 // Обработчик «отправки» формы
 function formSubmitHandler(evt) {
@@ -68,24 +68,58 @@ function closePopup() {
 }
 
 // Прикрепляем обработчик к форме:
-formElement.addEventListener('submit', formSubmitHandler);
+// formElement.addEventListener('submit', formSubmitHandler);
 
 // Закрытие окна
 btnClose.addEventListener('click', closePopup);
 
-buttonEditProfile.addEventListener('click', openPopup);
+buttonEditProfile.addEventListener('click', () => openForm1(formElement));
 
 function openForm(form) {
   form.classList.add('popup_opened');
-  const name = form.querySelector('.edit-form__input_type_name');
-  const link = form.querySelector('.edit-form__input_type_profession');
+  const name = form.querySelector('.edit-form__input_type_name').value;
+  const link = form.querySelector('.edit-form__input_type_profession').value;
   const btnSave = form.querySelector('.edit-form__button-save');
   const btnClose = form.querySelector('.popup__button-close');
 
   btnClose.addEventListener('click', () => form.classList.remove('popup_opened'));
 
+  // btnSave.addEventListener('click', function(evt) {
+  //   evt.preventDefault();
+  //   const photoCard = document.querySelector('#photo__card').content;
+  //   const card = photoCard.querySelector('.card').cloneNode(true);
+  //   card.querySelector('.card__image').src = link;
+  //   card.querySelector('.card__title').textContent = name;
+  //   photoCards.prepend(card);
+  // })
+
+
+
 
 }
+
+function openForm1(form) {
+  const name = form.querySelector('.edit-form__input_type_name');
+  const prof = form.querySelector('.edit-form__input_type_profession');
+  const btnSave = form.querySelector('.edit-form__button-save');
+  const btnClose = form.querySelector('.popup__button-close');
+
+    prof.value = editProfession.textContent;
+    name.value = editName.textContent;
+    popup.classList.add('popup_opened');
+
+  btnClose.addEventListener('click', () => form.classList.remove('popup_opened'));
+
+  form.addEventListener('submit', function formSubmitHandler(evt) {
+    evt.preventDefault();
+    editName.textContent = name.value;
+    editProfession.textContent = prof.value;
+    closeForm(form);
+  });
+
+}
+
+
 
 
 function closeForm(form) {
