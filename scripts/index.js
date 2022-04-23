d55c9fe127162ae3c4cf3947b1e4545b0f9ab519
000Ren000 <antonren@yandex.ru>
2022-04-23 21:36:18 +0700

    fix / Файлы с описанием классов принято называть с заглавной буквы Card.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-import {Card} from './card.js';
+import {Card} from './Card.js';
 import {FormValidator} from './FormValidator.js';
 
 const btnEditProfile = document.querySelector('.profile__edit-button');
