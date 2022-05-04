93fc6fb9e70d0f88291ca6c50bbaacc6e7ec66d6
000Ren000 <antonren@yandex.ru>
2022-05-04 11:00:02 +0700

    add / добавил клас section

--- scripts/Section.js
+++ scripts/Section.js
@@ -1,7 +1,22 @@
 export class Section {
-  constructor({items, render}, selector) {
+  constructor({items, renderer}, selector) {
     this._items = items;
-    this._render = render;
-    this._selector = document.querySelector(selector);
+    this._renderer = renderer;
+    this._container = document.querySelector(selector);
+  }
+
+  setItem(element) {
+    this._container.append(element);
+  }
+
+  clear() {
+    this._container.innerHTML = '';
+  }
+
+  renderItems() {
+    this.clear();
+    this._items.forEach(item => {
+      this._renderer(item);
+    });
   }
 }
