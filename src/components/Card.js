ed0d060483407b265c4c1af5beb02a1d2b646a63
000Ren000 <antonren@yandex.ru>
2022-05-15 14:02:41 +0700

    fix / Все обработчики для карточки нужно устанавливать в отдельном методе _setEventListeners

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,3 @@
-import PopupWithImage from './PopupWithImage.js';
 export class Card {
   constructor(cardDetails, selector, handleCardClick) {
     this._name = cardDetails.name;
@@ -8,24 +7,22 @@ export class Card {
       .querySelector('.card')
       .cloneNode(true);
     this._handleCardClick = handleCardClick;
+    this._btnLike = this._card.querySelector('.card__button-like');
+    this._btnRemoveCard = this._card.querySelector('.card__trash');
+    this._btnImage = this._card.querySelector('.card__image');
   }
 
-    _setEvents() {
-      const btnLike = this._card.querySelector('.card__button-like');
-      const btnRemoveCard = this._card.querySelector('.card__trash');
-      const btnImage = this._card.querySelector('.card__image');
-
+    _setEventListeners() {
       //Ставит лайк
-      btnLike.addEventListener('click',  () =>
-        btnLike.classList.toggle('card__button-like_active'));
-
+      this._btnLike.addEventListener('click',  () =>
+        this._btnLike.classList.toggle('card__button-like_active'));
       //Удаление карточки
-      btnRemoveCard.addEventListener('click',  () => {
+      this._btnRemoveCard.addEventListener('click',  () => {
         this._card.remove();
         this._card = null;
       })
      // Открывает картинку
-      btnImage.addEventListener('click',  () => {
+      this._btnImage.addEventListener('click',  () => {
         this._handleCardClick(this._link, this._name);
       });
     }
@@ -35,7 +32,7 @@ export class Card {
       _cardImage.src = this._link;
       _cardImage.alt = this._name;
       this._card.querySelector('.card__title').textContent = this._name;
-      this._setEvents(this._card);
+      this._setEventListeners(this._card);
       return this._card;
     }
 
